1dc2ea7f357fa6f335cbb9d27a37610d2f83c2c4
Trelorn <trelorn@ageswarcraft.io>
2018-09-08 19:56:17 -0400

    A brief explanation of what the build-customdeps.bash is for.

--- build-customdeps.bash
+++ build-customdeps.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+# You can put custom dependencies that you'd like to end up in the final Docker image in here. This is great for building deployment tooling specific to your server.
+
